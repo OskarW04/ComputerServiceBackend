58e7e20801e1362217bf4f0f8b6d96e2c02df1db
OskarW04 <oskarwitek04@gmail.com>
2026-01-14 12:23:13 +0100

    first admin created

--- src/main/java/com/example/ComputerService/config/SetupFirstAdmin.java
+++ src/main/java/com/example/ComputerService/config/SetupFirstAdmin.java
@@ -1,4 +1,24 @@
 package com.example.ComputerService.config;
 
-public class SetupFirstAdmin {
+import com.example.ComputerService.model.Employee;
+import com.example.ComputerService.model.enums.EmployeeRole;
+import com.example.ComputerService.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SetupFirstAdmin implements CommandLineRunner {
+    private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public void run(String ... args){
+        if(employeeRepository.findByEmail("admin@serwis.pl").isEmpty()){
+            employeeRepository.save(new Employee(null, "admin", "admin", "admin@serwis.pl", passwordEncoder.encode("admin123"), EmployeeRole.MANAGER, null));
+            System.out.println("Created new admin account");
+        }
+
+    }
 }
